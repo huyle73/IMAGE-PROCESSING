656776d0c21a05e1b6705c603540ffb23dfe828b
huyle73 <54856997+huyle73@users.noreply.github.com>
2020-07-22 22:59:05 +0700

    Update LUT.py

--- LUT.py
+++ LUT.py
@@ -1,37 +1,40 @@
 import cv2 as cv
 import numpy as np
+def calculate_LUT(cdf_gi,cdf_gj) :
+    lookup_table = np.zeros(256,1)
+    g_j = 0 #LUT value
+    for g_i in range (0,255):
+        while (cdf_gj < cdf_gi) & (g_j < 255):
+            g_j +=1
+        lookup_table[g_i +1] = g_j
+    return g_j
 
-def LUV(path = "/Users/lehuy/Desktop/01.jpeg"):
+
+def matching_histogram(path = "/Users/lehuy/Desktop/01.jpeg"):
     img = cv.imread(path)
     print(img.shape)
-    cv.imshow('before', img)
-    cv.waitKey(0)
-    cv.destroyWindow()
+    
 
+    #cv.imshow('before', img)
+    #cv.waitKey(0)
+    #cv.destroyWindow()
 
-    LUT = np.zeros(256,1)
-    g_j = 0
-    for g_i in range (0,256):
-        while cdf_gj() < cdf_gi():
-            g_j+=1
-        cv.LUT(g_i + 1) == g_j
-    return img
 
 def cdf_gi(img, g_i):
     count = len(img[img < g_i])
     sum = len(img) * len(img[0])
     p1 = count / sum;
     img[g_i+1] = p1
-    return img[g_i + 1]
+    return p1
 
 def cdf_gj(img, g_j):
     count = len(img[img < g_j])
     sum = len(img) * len(img[0])
     p2 = count / sum;
     img[g_j + 1] = p2
-    return img[g_j +1]
+    return p2
 
-img = LUV()
+img = matching_histogram()
 cv.imshow('after',img)
 cv.waitKey()
 cv.destroyWindow()
